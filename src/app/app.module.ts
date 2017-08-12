e30c2c4e7e478fa20794d879744e79109aebba19
redsanso <redsanso@gmail.com>
2017-08-12 15:58:26 +0200

    updated configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StartModule } from "./start/start.module";
+import { StartComponent } from "./start/start.component";
+
+let routes : Routes = [
+  { path : 'start', component : StartComponent, pathMatch : 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(routes),
+    StartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
