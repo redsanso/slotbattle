72ecf2e6170feb1776b764e23b17178295334c30
redsanso <redsanso@gmail.com>
2017-08-22 00:14:02 +0200

    basic scrollable slots

--- src/app/game-states/slots.state.ts
+++ src/app/game-states/slots.state.ts
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import {ListView} from 'phaser-list-view';
 import { GameState } from './game.state';
 
 export class SlotsState implements GameState {
@@ -6,9 +7,11 @@ export class SlotsState implements GameState {
     key: string = "slots";
 
     slots : Phaser.Image[] = [];
+    slotScrollers : ListView[] = [];
 
     preload = () => {
         this.game.load.image('slotbar', 'assets/png/Slotbar.png');
+        this.game.load.image('coin', 'assets/png/test-coin.png');
     }
 
     create = () => {
@@ -23,9 +26,22 @@ export class SlotsState implements GameState {
         ];
 
         this.slots.forEach((slot : Phaser.Image, index : number) => {
-            this.game.add
-                .tween(slot)
-                .to({ x : (slotsStartX + (100 * index)) }, 600, Phaser.Easing.Bounce.Out, true, 300 * index).start();
+            let slotTween = this.game.add.tween(slot)
+                .to({ x : (slotsStartX + (100 * index)) }, 600, Phaser.Easing.Bounce.Out, true, 300 * index);
+            slotTween.onComplete.add(() => {
+                let scrollRectangle = new Phaser.Rectangle((slotsStartX + (100 * index)), slotsStartY, slotsSrc.width, slotsSrc.height);
+                let listView = new ListView(this.game, this.game.world, scrollRectangle, {
+                    direction : 'y'
+                });
+
+                listView.add(this.game.add.sprite(0, scrollRectangle.y - 45, 'coin'));
+                listView.add(this.game.add.sprite(0, scrollRectangle.y + 45, 'coin'));
+                listView.add(this.game.add.sprite(0, scrollRectangle.y + 135, 'coin'));
+                listView.add(this.game.add.sprite(0, scrollRectangle.y + 225, 'coin'));
+                
+                this.slotScrollers.push(listView);
+            }, this);
+            slotTween.start();
         });
 
     } 
