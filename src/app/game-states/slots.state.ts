d43b65a50a82170fa1fbd6a4a61b786683d14e98
redsanso <redsanso@gmail.com>
2017-08-23 01:49:11 +0200

    5 slots

--- src/app/game-states/slots.state.ts
+++ src/app/game-states/slots.state.ts
@@ -7,8 +7,10 @@ export class SlotsState implements GameState {
     game : Phaser.Game;
     key: string = "slots";
 
+    coinHeight : number;
     coinSprites : Phaser.Image[] = [];
 
+    slotCount : number = 5;
     slots : Phaser.Image[] = [];
     slotScrollers : ListView[] = [];
 
@@ -24,20 +26,22 @@ export class SlotsState implements GameState {
 
     create = () => {
         let slotsSrc = this.game.cache.getImage('slotbar');
-        let slotsStartX = this.game.world.width - (slotsSrc.width * 3) - 20; // margin
+        let slotsStartX = this.game.world.width - (slotsSrc.width * this.slotCount) - 20; // margin
         let slotsStartY = Math.floor((this.game.world.height - slotsSrc.height) / 2);
 
-        this.slots = [
-            this.game.add.tileSprite(this.game.world.width, slotsStartY, slotsSrc.width, slotsSrc.height - 90, 'slotbar'),
-            this.game.add.tileSprite(this.game.world.width, slotsStartY, slotsSrc.width, slotsSrc.height - 90, 'slotbar'),
-            this.game.add.tileSprite(this.game.world.width, slotsStartY, slotsSrc.width, slotsSrc.height - 90, 'slotbar')
-        ];
+        let coinSrc = this.game.cache.getImage('coin0');
+        this.coinHeight = coinSrc.height;
+
+        this.slots = [];
+        for(var s = 0; s < this.slotCount; s++){
+            this.slots.push(this.game.add.tileSprite(this.game.world.width, slotsStartY, slotsSrc.width, slotsSrc.height - this.coinHeight, 'slotbar'));
+        }
 
         this.slots.forEach((slot : Phaser.Image, index : number) => {
             let slotTween = this.game.add.tween(slot)
                 .to({ x : (slotsStartX + (100 * index)) }, 600, Phaser.Easing.Bounce.Out, true, 300 * index);
             slotTween.onComplete.add(() => {
-                this.addPhaserListViewSlots((slotsStartX + (100 * index)), slotsStartY, slotsSrc.width, slotsSrc.height - 90);
+                this.addPhaserListViewSlots((slotsStartX + (100 * index)), slotsStartY, slotsSrc.width, slotsSrc.height - this.coinHeight);
             }, this);
             slotTween.start(); 
         });
@@ -69,7 +73,7 @@ export class SlotsState implements GameState {
         });
         listView.id = this.slotScrollers.length;
 
-        for(var y = -45, i = 0; i < 1000; y += 90, i++){
+        for(var y = -Math.floor(this.coinHeight / 2), i = 0; i < 100; y += this.coinHeight, i++){
             let randomCoin = Math.floor(Math.random() * this.coinSprites.length);
             let sprite = this.game.add.sprite(5, y, `coin${randomCoin}`);
             sprite.slotValue = randomCoin + 1;
@@ -77,7 +81,7 @@ export class SlotsState implements GameState {
         }
 
         listView.scroller.events.onComplete.add(() => {
-            let absPosition = Math.abs(listView.position) + 90;
+            let absPosition = Math.abs(listView.position) + this.coinHeight;
             let currentSprite = _.find(listView.items, (item) => item.position.y == absPosition);
             console.log(`Slot ${listView.id} has value ${currentSprite.slotValue}`);
         });
@@ -88,8 +92,8 @@ export class SlotsState implements GameState {
     runSlots(){
       this.slotScrollers.forEach((listView : ListView, index : number) => {
           let target = Math.floor(Math.random() * listView.length);
-          if((target - 45) % 90 > 0){
-              target -= target % 90;
+          if((target - Math.floor(this.coinHeight / 2)) % this.coinHeight > 0){
+              target -= target % this.coinHeight;
           }
 
           this.game.time.events.add((Phaser.Timer.SECOND * index / 2), () => {
